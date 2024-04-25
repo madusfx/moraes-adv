cb16b2a01b83f08531fe88e45914ed7a130cc5cb
madusfx <mariaesfreitas@gmail.com>
2024-04-25 10:55:06 -0300

    creating and styling header and menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,65 @@
-import './App.css';
+import {
+  Contact,
+  ContactText,
+  HamburguerMenu,
+  Header,
+  List,
+  ListItem,
+  Menu,
+  MenuDiv,
+  TextMenu,
+} from './styles';
+import Logo from './images/logo.svg';
+import { useState } from 'react';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function handleMenuOpen() {
+    if (menuOpen) {
+      setMenuOpen(false);
+    } else {
+      setMenuOpen(true);
+    }
+  }
+
+  function handleClick() {
+    const url =
+      'https://api.whatsapp.com/send?phone=554232237728&text=Ol%C3%A1,%20localizei%20o%20contato%20atrav%C3%A9s%20do%20site.%20Podemos%20conversar%3F';
+    window.open(url, '_blank');
+  }
+
   return (
     <div className="App">
-      <p>Moraes Advocacia</p>
+      <div style={{ display: 'flex' }}>
+        <Header>
+          <img src={Logo} alt="Logo da advocacia" />
+          <MenuDiv>
+            <TextMenu>MENU</TextMenu>
+            <HamburguerMenu open={menuOpen} onClick={handleMenuOpen}>
+              <div />
+              <div />
+              <div />
+            </HamburguerMenu>
+          </MenuDiv>
+        </Header>
+        <Contact onClick={handleClick}>
+          <ContactText>
+            ENTRE EM
+            <br /> CONTATO
+          </ContactText>
+        </Contact>
+      </div>
+      {menuOpen && (
+        <Menu>
+          <List>
+            <ListItem>SOBRE NÓS</ListItem>
+            <ListItem>ÁREAS DE ATUAÇÃO</ListItem>
+            <ListItem>EQUIPE</ListItem>
+            <ListItem>CONTATO</ListItem>
+          </List>
+        </Menu>
+      )}
     </div>
   );
 }
---
+++ src/styles.ts
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  height: 60px;
+  width: 90%;
+  background-color: #320000;
+  padding: 16px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  img {
+    margin-left: 64px;
+  }
+`;
+
+export const Contact = styled.div`
+  width: 10%;
+  background-color: #5d2417;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+export const ContactText = styled.p`
+  color: #fff;
+  font-weight: 700;
+  font-size: 16px;
+`;
+
+export const MenuDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TextMenu = styled.p`
+  color: #fff;
+  font-weight: 400;
+  font-size: 24px;
+`;
+
+export const HamburguerMenu = styled.div<{ open?: boolean }>`
+  width: 2rem;
+  height: 2rem;
+  margin-left: 12px;
+  display: flex;
+  justify-content: space-around;
+  flex-flow: column nowrap;
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background-color: #fff;
+    border-radius: 10px;
+    transform-origin: 1px;
+    transition: all 0.3s linear;
+
+    &:nth-child(1) {
+      transform: ${props => (props.open ? 'rotate(45deg)' : 'rotate(0)')};
+    }
+
+    &:nth-child(2) {
+      transform: ${props =>
+        props.open ? 'translateX(100%)' : 'translateX(0)'};
+      opacity: ${props => (props.open ? 0 : 1)};
+    }
+
+    &:nth-child(3) {
+      transform: ${props => (props.open ? 'rotate(-45deg)' : 'rotate(0)')};
+    }
+  }
+`;
+
+export const Menu = styled.div`
+  transition: all 0.3s linear;
+  margin-left: 69%;
+  top: 60px;
+  right: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #5d2417;
+  width: 20%;
+  padding: 12px;
+`;
+
+export const List = styled.ul``;
+
+export const ListItem = styled.li`
+  margin-top: 6px;
+  color: #fff;
+  font-weight: 400;
+  font-size: 16px;
+  text-align: center;
+  list-style-type: none;
+`;
