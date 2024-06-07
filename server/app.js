330b08eb9a5e5f5a843dc04ee8f0cdead99d6f30
madusfx <mariaesfreitas@gmail.com>
2024-06-07 12:00:16 -0300

    adding port env in the server

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const routes = require('./src/routes');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3030
 
 app.use(cors({
   origin: 'http://localhost:3000',
